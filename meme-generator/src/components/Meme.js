057a9416e2bd059ae8fa8f8f1a3f5b3092332145
silvamfpedro <silva.mfpedro@gmail.com>
2024-01-08 14:44:44 +0000

    Added meme images to meme generator
    Added meme image template to meme generator scrimba project folder

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,13 +1,17 @@
 import './Meme.css';
 import memesData from '../memesData';
+import React from 'react';
 
 function Meme() {
+
+  const [memeImage, setMemeImage] = React.useState("");
   
   function getMemeImg(){
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    
+    setMemeImage(memesArray[randomNumber].url);
+
+
   }
 
   return (
@@ -23,6 +27,9 @@ function Meme() {
             </div>
             <div>
                 <button className="form--button" onClick={getMemeImg}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={memeImage} className="meme--image" alt=""/>
             </div>
         </div>
     </main>
